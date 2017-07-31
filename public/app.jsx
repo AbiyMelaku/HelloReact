41915bf7f6d9f4b5a724d6a4a74052762b9e30f6
Abiy Melaku <agirma08@gmail.com>
2017-07-30 20:18:34 -0700

    Basics of practice React app
    just finished web pack config successfully

---
+++ public/app.jsx
@@ -0,0 +1,93 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var GreeterMessage = React.createClass({
+  render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
+    return (
+      <div>
+        <h1> Hello {name}!</h1>
+        <p> {message} </p>
+      </div>
+    );
+  }
+});
+
+var GreeterForm = React.createClass({
+  onFormSubmit: function (e) {
+    e.preventDefault();
+    
+    var updates = {};
+    var name = this.refs.name.value;
+    var message = this.refs.message.value;
+    
+    if( name.length > 0 ){
+      this.refs.name.value = '';
+      updates.name = name;
+    }
+    
+    if( message.length > 1 ){
+      this.refs.message.value = '';
+      updates.message = message;
+    }
+    
+    this.props.onNewState(updates);
+  },
+  
+  render: function() {
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <div>
+            <input type="text" placeholder="Enter name" ref="name"/>
+          </div>
+          <div>
+            <textarea placeholder="Enter message" ref="message"> </textarea>
+          </div>
+          <div>
+            <button> submit </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+});
+
+var Greeter = React.createClass({
+  getDefaultProps: function(){
+    return{
+      name: 'React',
+      message: 'this is the default message'
+    };  
+  },
+  
+  getInitialState: function(){
+    return{
+      name: this.props.name,
+      message: this.props.message
+    }
+  },
+  
+  handleNewState: function(updates) {
+   this.setState(updates);
+  },
+  
+  render: function(){
+    var name = this.state.name;
+    var message = this.state.message;
+    return(
+      <div>
+        <GreeterMessage name={name} message={message}/>
+        <GreeterForm onNewState={this.handleNewState}/>
+      </div>  
+    );
+  }
+  
+});
+
+
+ReactDOM.render(
+  <Greeter name="Abiy"/>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  
+  //specify the input
+  entry: './public/app.jsx',
+  
+  //specify the output
+  output: {
+    //set two properties on the object
+    path: __dirname,
+    filename:  './public/bundle.js'
+  },
+  
+  resolve: {
+    extension: ['', '.js', '.jsx']
+  },
+  
+  module: {
+    loaders:[
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  }
+};
