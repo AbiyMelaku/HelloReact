f3a2a18a960a5b835dbe237fa430433bd34d1344
Abiy Melaku <agirma08@gmail.com>
2017-08-13 14:28:27 -0700

    Modularize Greeter
    remove require statement of nested components

--- public/app.jsx
+++ public/app.jsx
@@ -1,42 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var GreeterMessage = require('./components/GreeterMessage');
-var GreeterForm = require('./components/GreeterForm');
+
+var Greeter = require('./components/Greeter');
 
 
 
-var Greeter = React.createClass({
-  getDefaultProps: function(){
-    return{
-      name: 'React',
-      message: 'this is the default message'
-    };  
-  },
-  
-  getInitialState: function(){
-    return{
-      name: this.props.name,
-      message: this.props.message
-    }
-  },
-  
-  handleNewState: function(updates) {
-   this.setState(updates);
-  },
-  
-  render: function(){
-    var name = this.state.name;
-    var message = this.state.message;
-    return(
-      <div>
-        <GreeterMessage name={name} message={message}/>
-        <GreeterForm onNewState={this.handleNewState}/>
-      </div>  
-    );
-  }
-  
-});
 
 
 ReactDOM.render(
